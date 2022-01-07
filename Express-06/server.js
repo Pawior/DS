45c3c43c938a89b966459bf4f57cf8a11f82b1d3
Pawior <pawiorx@gmail.com>
2022-01-07 08:37:31 +0100

    Heroku app update

--- Express-06/server.js
+++ Express-06/server.js
@@ -3,6 +3,9 @@ const app = express();
 const PORT = process.env.PORT || 3000; // bardzo istotna linijka - port zostaje przydzielony przez Heroku
 
 app.get("/", function (req, res) {
+  res.send("Heroku app - Wiktor Orda 3I1");
+});
+app.get("/data", function (req, res) {
   res.send({ imie: "Wiktor", nazwisko: "Orda", klasa: "3I1", grupa: "2" });
 });
 
