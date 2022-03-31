f2db22b1d0540d12fc5e0f3854e28e92918081cc
Pawior <pawiorx@gmail.com>
2022-03-31 22:25:18 +0200

    warcaby update

--- warcaby/static/scripts/Game.js
+++ warcaby/static/scripts/Game.js
@@ -11,8 +11,8 @@ class Game {
       [0, 1, 0, 1, 0, 1, 0, 1],
     ];
     this.pionki = [
-      [0, 2, 0, 2, 0, 2, 0, 2],
       [2, 0, 2, 0, 2, 0, 2, 0],
+      [0, 2, 0, 2, 0, 2, 0, 2],
       [0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0],
@@ -21,22 +21,50 @@ class Game {
       [0, 1, 0, 1, 0, 1, 0, 1],
     ];
     this.scene = new THREE.Scene();
-    this.camera = new THREE.PerspectiveCamera(45, 16 / 9, 0.1, 10000);
-    this.camera.position.set(0, 50, 200);
+    this.camera = new THREE.PerspectiveCamera(35, 16 / 9, 0.1, 10000);
+    this.camera.position.set(0, 100, 150);
     this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.rotation.x -= 0.7;
     this.camera.updateProjectionMatrix();
     this.renderer = new THREE.WebGLRenderer();
     this.renderer.setSize(window.innerWidth, window.innerHeight);
-    this.renderer.setClearColor(0x0066ff);
+    this.renderer.setClearColor(0xffffe6);
     // this.renderer.setSize(600, 600);
     document.getElementById("root").append(this.renderer.domElement);
     const axes = new THREE.AxesHelper(1000);
     this.scene.add(axes);
+    this.pickedPionek;
+
+    /**--------------------------------------------
+     *               Raycast
+     *---------------------------------------------**/
 
     this.createBoard();
     this.createPionek();
     this.render(); // wywołanie metody render
+    this.raycaster();
   }
+
+  raycaster = () => {
+    const raycaster = new THREE.Raycaster(); // obiekt Raycastera symulujący "rzucanie" promieni
+    const mouseVector = new THREE.Vector2();
+
+    document.addEventListener("mousedown", () => {
+      mouseVector.x = (event.clientX / window.innerWidth) * 2 - 1;
+      mouseVector.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+      raycaster.setFromCamera(mouseVector, this.camera);
+      const intersects = raycaster.intersectObjects(this.scene.children);
+      console.log(intersects.length);
+      if (intersects.length > 0) {
+        // zerowy w tablicy czyli najbliższy kamery obiekt to ten, którego potrzebujemy:
+        console.log(intersects[0].object);
+        this.pickedPionek = intersects[0].object;
+      } else {
+        console.log("nie wybrano pionka");
+      }
+    });
+  };
 
   createBoard = () => {
     let geometry = new THREE.BoxGeometry(10, 1, 10);
@@ -78,16 +106,21 @@ class Game {
       let tileX = -3.5;
       arr.forEach((elem, index) => {
         if (elem == 1) {
-          let pionek = new Pionek()
+          let pionek = new Pionek();
           pionek.setColor(0xffffff);
+          pionek.position.set(tileX * 10, 2, z);
+          this.scene.add(pionek);
+        } else if (elem == 2) {
+          let pionek = new Pionek();
+          pionek.setColor(0x85611b);
           pionek.position.set(tileX * 10, 2, z);
           this.scene.add(pionek);
         }
         tileX++;
-      })
+      });
       z = (i + 1) * 10;
-    })
-  }
+    });
+  };
 
   render = () => {
     requestAnimationFrame(this.render);
