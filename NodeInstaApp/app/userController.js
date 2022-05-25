20e362dd3def8284371bad52172fe6ca1f301b9a
Pawior <pawiorx@gmail.com>
2022-05-26 00:04:38 +0200

    Insta App authentication update: register, partially login are working

--- NodeInstaApp/app/userController.js
+++ NodeInstaApp/app/userController.js
@@ -1,114 +1,129 @@
-const { userArray } = require("./model")
-const bcrypt = require('bcrypt');
-const nodemailer = require("nodemailer")
-const jwt = require('jsonwebtoken');
+const { userArray } = require("./model");
+const bcrypt = require("bcrypt");
+const nodemailer = require("nodemailer");
+const jwt = require("jsonwebtoken");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const config = {
-    service: 'Yahoo',
-    auth: {
-        user: process.env.YAHOO_LOGIN,
-        pass: process.env.YAHOO_APP_PASS
-    }
-}
+  service: "Yahoo",
+  auth: {
+    user: process.env.YAHOO_LOGIN,
+    pass: process.env.YAHOO_APP_PASS,
+  },
+};
 
-
-console.log(config)
-const transporter = nodemailer.createTransport(config)
+console.log(config);
+const transporter = nodemailer.createTransport(config);
 
 // const pass = "moje tajne hasło"
 
 const encryptPass = async (password) => {
-
-    let encryptedPassword = await bcrypt.hash(password, 10);
-    console.log({ encryptedPassword: encryptedPassword });
-    return encryptedPassword
-}
+  let encryptedPassword = await bcrypt.hash(password, 10);
+  console.log({ encryptedPassword: encryptedPassword });
+  return encryptedPassword;
+};
+const decryptPass = async (userpass, encrypted) => {
+  let decrypted = await bcrypt.compare(userpass, encrypted);
+  console.log(decrypted);
+  return decrypted;
+};
 
 const sendMail = async (token) => {
-    transporter.sendMail({
-        from: "wiktor.orda@yahoo.com",
-        to: "wiktor.orda@yahoo.com",
-        subject: "subject",
-        text: "text",
-        html: `<b> Kliknij w poniższy link <br /> http://localhost:3000/api/user/confirm/${token} <br /> W celu potwierdzenia konta <br /> </b>`
-    });
-}
+  transporter.sendMail({
+    from: "wiktor.orda@yahoo.com",
+    to: "wiktor.orda@yahoo.com",
+    subject: "subject",
+    text: "text",
+    html: `<b> Kliknij w poniższy link <br /> http://localhost:3000/api/user/confirm/${token} <br /> W celu potwierdzenia konta <br /> </b>`,
+  });
+};
 
 const createToken = async (userObj) => {
-    const jwt = require('jsonwebtoken');
-    let token = await jwt.sign(
-        {
-            name: userObj.name,
-            email: userObj.email,
-            anyData: "123"
-        },
-        process.env.SECRET_KEY, // powinno być w .env
-        {
-            expiresIn: "2m" // "1m", "1d", "24h"
-        }
-    );
-    console.log({ token: token });
-    return token
-}
-
+  const jwt = require("jsonwebtoken");
+  let token = await jwt.sign(
+    {
+      id: userArray.length,
+      name: userObj.name,
+      email: userObj.email,
+      anyData: "123",
+    },
+    process.env.SECRET_KEY, // powinno być w .env
+    {
+      expiresIn: "5m", // "1m", "1d", "24h"
+    }
+  );
+  console.log({ token: token });
+  return token;
+};
 
 const verifyToken = async (token) => {
-
-    try {
-        let decoded = await jwt.verify(token, "verysecretkey")
-        console.log({ decoded: decoded });
-    }
-    catch (ex) {
-        console.log({ message: ex.message });
-    }
-}
-
+  try {
+    let decoded = await jwt.verify(token, "verysecretkey");
+    console.log({ decoded: decoded });
+    return decoded;
+  } catch (ex) {
+    console.log({ message: ex.message });
+  }
+};
 
 const processToken = async (token) => {
-    await verifyToken(token)
-}
+  return await verifyToken(token);
+};
 
 // processToken(token)
-
-
 
 // encryptPass(pass)
 
 module.exports = {
-    getAll: (req) => {
-        // delete require.cache[require.resolve("./fileController")];
-        // let { imagesArray } = require("./fileController.js");
-        // console.log(newImagesArray);
-        console.log(imagesArray);
-        return imagesArray;
-    },
-    registerUser: async (userData) => {
-        let parsedUserData = JSON.parse(userData);
-        let encryptedPass = await encryptPass(parsedUserData.password)
-        let userObj = {
-            id: userArray.length + 1,
-            name: parsedUserData.name,
-            lastName: parsedUserData.lastName,
-            email: parsedUserData.email,
-            confirmed: false,
-            password: encryptedPass
-        }
-        console.log(userObj)
-        userArray.push(userObj)
-        let generatedToken = await createToken(userObj)
-        try {
-            await sendMail(generatedToken)
-        }
-        catch (e) {
-            console.log(e)
-        }
-
-    },
-    confirmAccount: async (token) => {
-        console.log("token: " + token)
-        await processToken(token)
-
+  getAll: (req) => {
+    // delete require.cache[require.resolve("./fileController")];
+    // let { imagesArray } = require("./fileController.js");
+    // console.log(newImagesArray);
+    console.log(userArray);
+    return userArray;
+  },
+  registerUser: async (userData) => {
+    let parsedUserData = JSON.parse(userData);
+    let encryptedPass = await encryptPass(parsedUserData.password);
+    let userObj = {
+      id: userArray.length + 1,
+      name: parsedUserData.name,
+      lastName: parsedUserData.lastName,
+      email: parsedUserData.email,
+      confirmed: false,
+      password: encryptedPass,
+    };
+    console.log(userObj);
+    userArray.push(userObj);
+    let generatedToken = await createToken(userObj);
+    try {
+      await sendMail(generatedToken);
+    } catch (e) {
+      console.log(e);
     }
+  },
+  confirmAccount: async (token) => {
+    let decodedToken = await processToken(token);
+    console.log("token: " + token);
+    console.log(decodedToken);
+    const found = userArray.find((user) => user.id == decodedToken.id);
+    console.log(found);
+    found.confirmed = true;
+  },
+  loginUser: async (userData) => {
+    let parsedUserData = JSON.parse(userData);
+
+    const foundUser = userArray.find(
+      (user) => user.email == parsedUserData.email
+    );
+    console.log(foundUser);
+    let passMatch = await decryptPass(
+      parsedUserData.password,
+      foundUser.password
+    );
+    if (passMatch) {
+      console.log("hasło się zgadza");
+    }
+  },
 };
--- NodeInstaApp/app/userRouter.js
+++ NodeInstaApp/app/userRouter.js
@@ -2,26 +2,25 @@ const userController = require("./userController");
 const getRequestData = require("./utils.js");
 
 const userRouter = async (req, res) => {
-    if (req.url == "/api/user" && req.method == "GET") {
-        let data = await getRequestData(req);
-        // console.log(tagsController.getAllRaw(data));
-        res.end(JSON.stringify(userController.getAll(), null, 5));
-    } else if (
-        req.url.match(/\/api\/user\/confirm\/([0-9a-zA-Z]+)/) &&
-        req.method == "GET"
-    ) {
-        let reqUrl = req.url;
-        let token = reqUrl.substring(reqUrl.lastIndexOf("/") + 1);
-        // console.log(tagsController.getAllObj(data));
-        res.end(JSON.stringify(userController.confirmAccount(token), null, 5));
-    } else if (req.url == "/api/user/login" && req.method == "POST") {
-        let data = await getRequestData(req);
-        // console.log(tagsController.getAllObj(data));
-        res.end(JSON.stringify(userController.getAllObj(data), null, 5));
-    }
-    else if (req.url == "/api/user/register" && req.method == "POST") {
-        let data = await getRequestData(req);
-        userController.registerUser(data);
-    }
+  if (req.url == "/api/user" && req.method == "GET") {
+    let data = await getRequestData(req);
+    // console.log(tagsController.getAllRaw(data));
+    res.end(JSON.stringify(userController.getAll(), null, 5));
+  } else if (
+    req.url.match(/\/api\/user\/confirm\/([0-9a-zA-Z]+)/) &&
+    req.method == "GET"
+  ) {
+    let reqUrl = req.url;
+    let token = reqUrl.substring(reqUrl.lastIndexOf("/") + 1);
+    // console.log(tagsController.getAllObj(data));
+    res.end(JSON.stringify(userController.confirmAccount(token), null, 5));
+  } else if (req.url == "/api/user/login" && req.method == "POST") {
+    let data = await getRequestData(req);
+    // console.log(tagsController.getAllObj(data));
+    res.end(JSON.stringify(userController.loginUser(data), null, 5));
+  } else if (req.url == "/api/user/register" && req.method == "POST") {
+    let data = await getRequestData(req);
+    userController.registerUser(data);
+  }
 };
 module.exports = userRouter;
